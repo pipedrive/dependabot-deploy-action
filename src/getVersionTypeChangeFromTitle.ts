254f8bb7cbad35dcc2482c400bcede61931d3b06
Lukas Pastuszek <lukas.pastuszek@pipedrive.com>
2020-04-19 23:23:10 +0200

    Simplify and tes better the logic to get version from title

--- src/getVersionTypeChangeFromTitle.ts
+++ src/getVersionTypeChangeFromTitle.ts
@@ -1,4 +1,5 @@
 import { VersionType } from "./types";
+import semver from 'semver-diff';
 
 export const getVersionTypeChangeFromTitle = (title: string): VersionType => {
     const VERSIONS_REGEX = /[0-9.]+/g;
@@ -7,31 +8,20 @@ export const getVersionTypeChangeFromTitle = (title: string): VersionType => {
       throw new Error(`Expected two versions in PR title "${title}"`);
     }
 
-    const previous = versions[0].split('.').map(Number)
-    const next = versions[1].split('.').map(Number)
+    const [previous, next] = versions;
 
-    if (previous.length !== 3) {
-      throw new Error(`Expected previous version to be in format X.X.X. Found "${versions[0]}"`);
-    }
-    if (next.length !== 3) {
-      throw new Error(`Expected next version to be in format X.X.X. Found "${versions[1]}"`);
-    }
+    const version = semver(previous, next);
 
-    if (previous[0] >= next[0] && previous[1] >= next[1] && previous[2] >= next[2]) {
-      throw new Error(`Expected previous version to be smaller in PR title "${title}"`);
-    }
-
-    if (next[0] > previous[0]) {
-      return 'MAJOR';
+    switch(version) {
+      case 'patch':
+        return 'PATCH';
+      case 'minor':
+        return 'MINOR';
+      case 'major':
+        return 'MAJOR';
+      default:
+        throw new Error(`Unexpected version change ${version} for title ${title}`);
     }
 
-    if (next[1] > previous[1]) {
-      return 'MINOR'
-    }
-
-    if (next[2] > previous[2]) {
-      return 'PATCH'
-    }
-
-    throw new Error(`Unexpected case for title ${title}`);
+    
 }
